2c0b24f7cd55438cb2932778cbc72cbfe23079b9
Tiziano <tiziano.michelessi@gmail.com>
2025-06-16 18:22:21 +0200

    Add shifts router to main application for enhanced API functionality

--- shifty/main.py
+++ shifty/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel
-from shifty.api.routers import availabilities
+from shifty.api.routers import availabilities, shifts
 from shifty.infrastructure.db import engine
 
 SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
 app.include_router(availabilities.router)
+app.include_router(shifts.router)
 
 @app.get("/")
 async def root():
